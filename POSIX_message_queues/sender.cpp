dc2d7a2ecd64321d94abd8550369513896a494cc
Angellyn218 <angel.lyn.cervantes@gmail.com>
2023-12-17 20:27:00 -0800

    added print statements for debugging

--- POSIX_message_queues/sender.cpp
+++ POSIX_message_queues/sender.cpp
@@ -29,13 +29,6 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
-    // Open the file for reading
-    fileSend = fopen(argv[1], "rb");
-    if (fileSend == NULL) {
-        perror("fopen");
-        exit(-1);
-    }
-
     // Let's create the queue
     myQueue = mq_open(MQ_NAME, O_CREAT | O_RDWR, 0744, &attr);
     if (myQueue < 0) {
@@ -43,18 +36,31 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
-    // Read file and send content through the message queue
-    size_t bytesRead;
-    while ((bytesRead = fread(messageBuff, 1, MAX_READ_SIZE, fileSend)) > 0) {
-        // Send the bytes read through the message queue
-        retVal = mq_send(myQueue, messageBuff, bytesRead, 1);
+	// Open the file for reading
+    fileSend = fopen(argv[1], "rb");
+	if (fileSend == NULL) {
+		perror("fopen");
+		exit(-1);
+	}
 
-        // Send file sanity check
-        if (retVal == -1) {
-            perror("mq_send");
-            exit(-1);
-        }
-    }
+	printf("File opened successfully.\n");
+
+	// Read file and send content through the message queue
+	size_t bytesRead;
+	while ((bytesRead = fread(messageBuff, 1, MAX_READ_SIZE, fileSend)) > 0) {
+		printf("Read %zu bytes from the file.\n", bytesRead);
+
+		// Send the bytes read through the message queue
+		retVal = mq_send(myQueue, messageBuff, bytesRead, 1);
+
+		// Send file sanity check
+		if (retVal == -1) {
+			perror("mq_send");
+			exit(-1);
+		}
+
+		printf("Sent %zu bytes through the message queue.\n", bytesRead);
+	}
 
     // Close the file and message queue after sending data
     fclose(fileSend);
