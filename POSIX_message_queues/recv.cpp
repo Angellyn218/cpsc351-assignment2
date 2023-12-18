ab372c861bf619abe0a1c591ef7e709eabc3bd7e
Angellyn218 <angel.lyn.cervantes@gmail.com>
2023-12-17 20:12:56 -0800

    Debugging code

--- POSIX_message_queues/recv.cpp
+++ POSIX_message_queues/recv.cpp
@@ -8,73 +8,53 @@
 
 #define MQ_MSGSIZE 5000
 #define MQ_NAME "/cpsc351queue"
+#define FILENAME "file__recv"
 
-int main()
-{
-	// The message queue descriptor and the return value
-	mqd_t myQueue, retVal;
-	
-	// The message queue attributes
-	mq_attr attr;
-	
-	// The message buffer
-	char messageBuff[MQ_MSGSIZE];
-	
-	unsigned int priority;	
-	
-	// Initialize the attributes
-	attr.mq_flags = 0;
-	attr.mq_maxmsg = 10;
-	attr.mq_msgsize = 4096;
-	attr.mq_curmsgs = 0;
-	
-	// Let's create the queue
-	myQueue = mq_open(MQ_NAME, O_RDWR, 0744, 0);
-	
-	// Sanity checks
-	if(myQueue < 0)
-	{
-		perror("mq_open");
-		exit(-1);
-	}
+int main() {
+    mqd_t myQueue, retVal;
+    mq_attr attr;
+    char messageBuff[MQ_MSGSIZE];
 
-	while (1) {
-		// NOTE: the MQ_MSGSIZE must be greater than
-		// the mq_msgsize field of the attribute struct
-		// used to create the queue	
-		retVal = mq_receive(myQueue, messageBuff, MQ_MSGSIZE, NULL);
-		
-		// Sanity checks
-		if(retVal == -1)
-		{
-			perror("mq_receive");
-			exit(-1);
-		}
+    // Initialize message queue attributes
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 4096;
+    attr.mq_curmsgs = 0;
 
-		if (retVal > 0) {
-			// open file
-			FILE *fileRecv = fopen("file__recv", "a");
+    // Create or open the queue
+    myQueue = mq_open(MQ_NAME, O_RDWR, 0744, &attr);
 
-			if (fileRecv == NULL) {
-				perror("fopen");
-				exit(-1);
-			}
+    if (myQueue < 0) {
+        perror("mq_open");
+        exit(-1);
+    }
 
-			// Write the message content to the file
-			fwrite(messageBuff, 1, retVal, fileRecv);
-			// (stderr, "%s\n", messageBuff);
+    FILE *fileRecv = fopen(FILENAME, "a");
+    if (fileRecv == NULL) {
+        perror("fopen");
+        exit(-1);
+    }
 
-			// Close the file
-			fclose(fileRecv);
+    while (1) {
+        retVal = mq_receive(myQueue, messageBuff, MQ_MSGSIZE, NULL);
 
-		} else {
-			// Close the queue
-			mq_close(myQueue);
-			mq_unlink(MQ_NAME);
-			exit(0);
-		}
-		
-	}
+        if (retVal == -1) {
+            perror("mq_receive");
+            exit(-1);
+        }
 
-	return 0;
+        if (retVal > 0) {
+            // Write the message content to the file
+            fwrite(messageBuff, 1, retVal, fileRecv);
+        } else {
+            // Close the file and queue before exiting
+            fclose(fileRecv);
+            mq_close(myQueue);
+            mq_unlink(MQ_NAME);
+            exit(0);
+        }
+    }
+
+    // This part of the code is unreachable
+    return 0;
 }
--- POSIX_message_queues/sender.cpp
+++ POSIX_message_queues/sender.cpp
@@ -9,77 +9,69 @@
 #define MAX_READ_SIZE 4096
 #define MQ_NAME "/cpsc351queue"
 
-int main(int argc, char** argv)
-{
-	
-	// The message queue descriptor and the return value
-	mqd_t myQueue, retVal;
-
-	// Define Send File and Message Buff
-	FILE *fileSend;
+int main(int argc, char** argv) {
+    mqd_t myQueue, retVal;
+    FILE *fileSend;
     char messageBuff[MAX_READ_SIZE];
-	
-	// The message queue attributes
-	mq_attr attr;
-	
-	// Initialize the attributes
-	attr.mq_flags = 0;
-	attr.mq_maxmsg = 10;
-	attr.mq_msgsize = 4096;
-	attr.mq_curmsgs = 0;
-		
-	// Sanity checks
-	if(argc != 2)
-	{
-		// fprintf(stderr, "USAGE: %s <message to send>\n");
-		exit(-1);
-	}
-		
-	// Let's create the queue
-	myQueue = mq_open(MQ_NAME, O_CREAT | O_RDWR, 0744, &attr);
-	
-	// Sanity checks
-	if(myQueue < 0)
-	{
-		perror("mq_open");
-		exit(-1);
-	}
 
-	// Open the send file
+    // The message queue attributes
+    struct mq_attr attr;
+
+    // Initialize the attributes
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 4096;
+    attr.mq_curmsgs = 0;
+
+    // Sanity checks
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: %s <file name>\n", argv[0]);
+        exit(-1);
+    }
+
+    // Let's create the queue
+    myQueue = mq_open(MQ_NAME, O_CREAT | O_RDWR, 0744, &attr);
+
+    // Sanity checks
+    if (myQueue < 0) {
+        perror("mq_open");
+        exit(-1);
+    }
+
+    // Open the send file
     fileSend = fopen(argv[1], "rb");
     if (fileSend == NULL) {
         perror("fopen");
         exit(-1);
     }
 
-	// Read file 
-	while (!feof(fileSend)) {
-        // Read at most MAX_READ_SIZE bytes from the file
-        size_t bytesRead = fread(messageBuff, 1, MAX_READ_SIZE, fileSend);
-
+    // Read file and send content through the message queue
+    size_t bytesRead;
+    while ((bytesRead = fread(messageBuff, 1, MAX_READ_SIZE, fileSend)) > 0) {
         // Send the bytes read through the message queue
         retVal = mq_send(myQueue, messageBuff, bytesRead, 1);
 
-		// Send file sanity check
+        // Send file sanity check
         if (retVal == -1) {
             perror("mq_send");
             exit(-1);
         }
     }
 
-	// Empty message for indication of end of file 
-	retVal = mq_send(myQueue, "", 0, 2);
-	
-	// Send file sanity checks
-	if(retVal == -1)
-	{
-		perror("mq_send");
-		exit(-1);
-	}
+    // Close file
+    fclose(fileSend);
 
-	// Close file and the message queue
-	fclose(fileSend);
-	mq_close(myQueue);
-		
-	return 0;
+    // Empty message for indication of end of file
+    retVal = mq_send(myQueue, "", 0, 2);
+
+    // Send file sanity checks
+    if (retVal == -1) {
+        perror("mq_send");
+        exit(-1);
+    }
+
+    // Close the message queue after sending the end-of-file indicator
+    mq_close(myQueue);
+
+    return 0;
 }
